7f03e8c993bef349c91ec0d77ff5ea0e4e7b6d43
Ryan L. Gonzales <ryan.gonzales@pmti.local>
2018-02-21 18:01:22 +0800

    initial commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase : {
+    apiKey: "AIzaSyBoC7QlIxMf1bQ33UNEpvB1izKux3tc4-A",
+    authDomain: "todolistapp-9ba95.firebaseapp.com",
+    databaseURL: "https://todolistapp-9ba95.firebaseio.com",
+    projectId: "todolistapp-9ba95",
+    storageBucket: "todolistapp-9ba95.appspot.com",
+    messagingSenderId: "1030551811866"
+  }
 };
