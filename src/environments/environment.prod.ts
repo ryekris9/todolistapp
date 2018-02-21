2c82744b242be87d49d6ff7e1b87bb4953f1b6f5
Ryan L. Gonzales <ryan.gonzales@pmti.local>
2018-02-21 18:24:16 +0800

    first commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase : {
+    apiKey: "AIzaSyBoC7QlIxMf1bQ33UNEpvB1izKux3tc4-A",
+    authDomain: "todolistapp-9ba95.firebaseapp.com",
+    databaseURL: "https://todolistapp-9ba95.firebaseio.com",
+    projectId: "todolistapp-9ba95",
+    storageBucket: "todolistapp-9ba95.appspot.com",
+    messagingSenderId: "1030551811866"
+  }
 };
